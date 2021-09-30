b22f969831921eb6c9532a154eabf49760478082
OOI YONG JUN <eugeneooi9155@gmail.com>
2021-09-30 23:21:12 +0800

    Add files via upload

--- register.php
+++ register.php
@@ -1,4 +1,28 @@
 <?php
+require_once('config.php');
+?>
+
+<?php
+
+	if(!isset($_SESSION))
+		session_start();
+	
+	if(isset($_POST["signup"])){
+		$uname = $_POST["newName"];
+		$email = $_POST["newEmail"];
+		$pass = $_POST["newPassword"];
+		
+		$sql = "INSERT INTO user(Username, Email, Password) VALUES(?,?,?)";
+		$stmtinsert = $db->prepare($sql);
+		$names=array($uname, $email, $pass);
+		$result = $stmtinsert->execute([$uname, $email, $pass]);
+		if ($result){
+			echo 'Successfully saved.';
+		}else{
+			echo 'There were errors while saving.';
+		}
+	}
+	
 	include('header.php');
 ?>
 
@@ -11,33 +35,37 @@
 	
 	
 	<!-- Register -->
-	<div class="container" id="registerNew">
+	<div>
+	<form action ="register.php" method="post">
+		<div class="container" id="registerNew">
 		
-		<h3>Enter Your Name</h3>
-		<input type="text" name="newName" placeholder="Enter your Name">
+			<h3>Enter Your Username</h3>
+			<input type="text" name="newName" placeholder="Enter your username" required>
+			<br>
+			<h3>Enter Your Email</h3>
+			<input type="email" name="newEmail" placeholder="Enter your email"required>
+			<br>
+			<h3>Enter Your Password</h3>
+			<input type="password" name="newPassword" placeholder="Enter your password"required>
+			<br>
+			<br>
+			<div class="col-md-4 col-sm-12">
+				<input type="submit" class="form-control" name="signup" value="Sign Up">
+			</div>
 		<br>
-		<h3>Enter Your Email</h3>
-		<input type="email" name="newEmail" placeholder="Enter your email">
 		<br>
-		<h3>Enter Your Password</h3>
-		<input type="text" name="newPassword" placeholder="New Password">
-		<br>
-		<br>
-		<div class="col-md-4 col-sm-12">
-			<input type="submit" class="form-control" name="signup" value="Sign Up">
 		</div>
-		<br>
-		<br>
-		
+	</form>
 	</div>
-
 
 	<!-- Login -->
 	<div class="container" id="loginTransfer">
 		<br>
 		<h3>Already a member?</h3>
 		<div class="col-md-4 col-sm-12">
+			<a href="login.php">
 			<input type="submit" class="form-control" name="loginTransfer" value="Log In">
+			</a>
 		</div>
 		<br>
 		<br>
