05ee8b07bd5674030a924d42182c7e41d7d683b6
tungmunwah29 <75323646+tungmunwah29@users.noreply.github.com>
2021-11-20 16:40:49 +0800

    Update Test_AddGame.php

--- UnitTestCase/test/Test_AddGame.php
+++ UnitTestCase/test/Test_AddGame.php
@@ -7,49 +7,6 @@ class Test_AddGame extends TestCase
 {
     /** @group db */
 
-    public function test_user_add_gamenumber_are_different(){
-        $mockRepo = $this->createMock(AddRepository::class);
-
-        $mockGamesArray=[
-            ['gameID'=>11,
-                'Name'=>'Testing',
-                'Publisher'=> 'Tung',
-                'Year'=> 1999,
-                'Description' => 'Its the best game',
-                'Date'=>'2021-10-18 22:45:28',
-                'user' => 'MunWah'
-            ]
-        ];
-
-        $newGame=[
-
-            ['gameID'=>12,
-                'Name'=>'Testing1',
-                'Publisher'=> 'Tung1',
-                'Year'=> 2000,
-                'Description' => 'Its the worst game',
-                'Date'=>'2021-10-18 22:45:28',
-                'user' => 'MunWah1'
-            ]
-        ];
-
-        $mockRepo->expects($this->once())->method('fetchGames')->willReturn($mockGamesArray);
-
-        $games = $mockRepo->fetchGames();
-
-        try{
-            $this->assertNotEquals($newGame[0]['gameID'],$games[0]['gameID']);
-            echo'GameID number is not the same.';
-//
-        }catch (Exception $error){
-            echo $error->getMessage();
-            $this->fail("GameID is the same");
-
-
-        }
-
-    }
-
 
     public function test_user_gameID_are_different(){
         $mockRepo = $this->createMock(AddRepository::class);
